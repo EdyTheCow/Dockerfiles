9abedf7129640040fd9b0e869f3f09d16c9f17eb
edyyy <github@edyyy.com>
2017-05-20 14:35:05 +0200

    update

--- minecraft/moodpack/common/entrypoint.sh
+++ minecraft/moodpack/common/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # server.properties
-echo max-players=${MC_MAX_PLAYERS:-20} > server.properties
+echo max-players=${MC_MAX_PLAYERS:-30} > server.properties
 echo server-port=${MC_PORT:-25565} >> server.properties
 echo view-distance=${MC_VIEW_DISTANCE:-3} >> server.properties
 
