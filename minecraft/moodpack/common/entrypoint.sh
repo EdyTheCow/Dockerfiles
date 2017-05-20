1db85b05aee16de525d3272ea0ff730d3c9d0333
edyyy <github@edyyy.com>
2017-05-20 14:05:35 +0200

    ram variables

--- minecraft/moodpack/common/entrypoint.sh
+++ minecraft/moodpack/common/entrypoint.sh
@@ -5,4 +5,4 @@ echo max-players=${MC_MAX_PLAYERS:-20} > server.properties
 echo server-port=${MC_PORT:-25565} >> server.properties
 echo view-distance=${MC_VIEW_DISTANCE:-3} >> server.properties
 
-/usr/bin/java -Xms${XMS:-512M} -Xmx${XMS:-3G} -XX:+UseConcMarkSweepGC -jar thermos-1.7.10-1614-server.jar
+/usr/bin/java -Xms${RAM_XMS:-512M} -Xmx${RAM_XMX:-3G} -XX:+UseConcMarkSweepGC -jar thermos-1.7.10-1614-server.jar
